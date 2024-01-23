cd98afe349fc8ee23f6d9e461e4fb5337a71bdad
Gabe Granado <gabegranado@Gabes-MacBook-Pro.local>
2024-01-22 19:31:17 -0600

    added mongodb connection along with env enviroment handler to app and dockerfiles

--- server/app.js
+++ server/app.js
@@ -1,19 +1,47 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import sprintf from 'sprintf';
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
+dotenv.config();
 const PORT = 4000;
 const app = express();
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const uri = sprintf("mongodb+srv://casgrana:%s@cluster0.jlyjhgq.mongodb.net/?retryWrites=true&w=majority", DB_PASSWORD);
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    console.log("get test res")
     res.json([
         {
             "test": "testgi"
         }
     ])
 })
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(PORT, function() {
     console.log('running the server on port ' + PORT);
