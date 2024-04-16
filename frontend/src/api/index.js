e178a1ee8b017e1df2759d39694b76d74d719327
Gabe Granado <gabegranado@Gabes-MacBook-Pro.local>
2024-04-16 18:47:29 -0500

    added api functionalities and test page to show how it works

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export const getStreams = () => axios.get('http://localhost:3000/streams');
+export const getStreams = () => axios.get('http://localhost:4000/streams');
--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,9 +1,31 @@
-
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getStreams } from '../actions/streams.js';
 
 export default function Test() {
+    const dispatch = useDispatch();
+    const streams = useSelector((state) => state.streams);
+    const [shownStreams, setShownStreams] = useState([]);
+
+    useEffect(() => {
+        dispatch(getStreams());
+    }, [dispatch]);
+
+    useEffect(() => {
+        const streamSelection = Object.values(streams).filter(stream => stream.streamLink);
+        setShownStreams(streamSelection);
+    }, [streams]);
+
     return (
         <div>
-            test
+            <h1>streams</h1>
+            <ul>
+                {shownStreams.map(stream => (
+                    <li key={stream.id}>
+                        <a href={stream.streamLink}>{stream.title} {stream.gameCategory}</a>
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
