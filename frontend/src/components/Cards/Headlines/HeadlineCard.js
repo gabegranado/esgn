2506a084630961c4af064c0fe926aa7af785c438
Gabe Granado <gabegranado@Gabes-MacBook-Pro.local>
2024-04-18 18:37:17 -0500

    idk

--- frontend/src/components/Cards/Headlines/HeadlineCard.js
+++ frontend/src/components/Cards/Headlines/HeadlineCard.js
@@ -1,35 +1,35 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 import Paper from "@material-ui/core/Paper";
 
 export default function HeadlineCard({ headlineData }) {
   return (
-    <a href="http://localhost:3000/" style={{ textDecoration: 'none' }}>
-        <Paper>
-            <Card>
-            <CardActionArea>
+    <a href="http://localhost:3000/" style={{ textDecoration: "none" }}>
+      <Paper>
+        <Card>
+          <CardActionArea>
             <CardMedia
-                component="img"
-                height="140"
-                image="/GAMERS.jpeg"
-                alt="GAMERS"
+              component="img"
+              height="140"
+              image="/GAMERS.jpeg"
+              alt="GAMERS"
             />
             <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {headlineData.headline}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
+              <Typography gutterBottom variant="h5" component="div">
+                {headlineData.headline}
+              </Typography>
+
+              <Typography variant="body2" color="text.secondary">
                 {headlineData.websiteLink}
-                </Typography>
+              </Typography>
             </CardContent>
-            </CardActionArea>
+          </CardActionArea>
         </Card>
-        </Paper>
-      
+      </Paper>
     </a>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,73 +1,96 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getArticles } from '../actions/headlines.js';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getArticles } from "../actions/headlines.js";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import HeadlineCard from '../components/Cards/Headlines/HeadlineCard.js';
-import ScoreCard from '../components/Cards/Score/ScoreCard.js';
-import Typography from '@mui/material/Typography';
-import { createTheme, responsiveFontSizes } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import HeadlineCard from "../components/Cards/Headlines/HeadlineCard.js";
+import ScoreCard from "../components/Cards/Score/ScoreCard.js";
+import Typography from "@mui/material/Typography";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 
 export default function Test() {
-      
-    const dispatch = useDispatch();
-    const headlines = useSelector((state) => state.headlines);
-    const [shownHeadlines, setShownHeadlines] = useState([]);
+  const dispatch = useDispatch();
+  const headlines = useSelector((state) => state.headlines);
+  const [shownHeadlines, setShownHeadlines] = useState([]);
 
-    useEffect(() => {
-        dispatch(getArticles());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getArticles());
+  }, [dispatch]);
 
-    useEffect(() => {
-        const headlinesSelection = Object.values(headlines).filter(headline => headline.headline);
-        setShownHeadlines(headlinesSelection);
-    }, [headlines]);
-
-    return (
-        <div style={{  backgroundColor: '#f0f0f0', minHeight: '100vh', paddingTop: '20px', paddingRight: '2px', paddingLeft: '2px'}}>
-            <Grid container spacing={2}>
-                
-            <Grid item xs={12} sm={2} md={3}>
-
-                <Grid container justifyContent="center" alignItems="center" spacing={2}>
-    {shownHeadlines.map((headline, index) => (
-        <Grid item key={headline.id}>
-            {index === 0 && (
-                <div style={{paddingBottom: '10px'}}>
-                    <img src="/scores.png" alt="Scores" style={{ maxWidth: '100%', height: 'auto' }} />
-                </div>
-            )}
-            <ScoreCard />
-        </Grid>
-    ))}
-</Grid>
-
-                </Grid>
-
-
-                <Grid item xs={12} sm={6} md={6}>
-                    <Grid container justifyContent="center" alignItems="center" spacing={2}>
-
-                        <Grid item>
-                            mainStream
-                        </Grid>
-                        
-                        {shownHeadlines.map(headline => (
-                            <Grid item key={headline.id}>
-                                <HeadlineCard headlineData={headline}/>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Grid>
-
-                <Grid item xs={12} sm={6} md={3}>
-                    <Paper>right</Paper>
-                </Grid>
-
-            </Grid>
-        </div>
+  useEffect(() => {
+    const headlinesSelection = Object.values(headlines).filter(
+      (headline) => headline.headline
     );
+    setShownHeadlines(headlinesSelection);
+  }, [headlines]);
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        paddingTop: "20px",
+        paddingRight: "2px",
+        paddingLeft: "2px",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={2} md={3}>
+          <div style={{ paddingBottom: "10px" }}>
+            <img
+              src="/scores.png"
+              alt="Scores"
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
+          </div>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            {shownHeadlines.map((headline, index) => (
+              <Grid item key={headline.id}>
+                <ScoreCard scoreData={headline} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={6}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item>
+              <iframe
+                width="860"
+                height="515"
+                src="https://www.youtube.com/embed/iUFvGIORfAY?si=Fkab1X6R5K67u8sH"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              ></iframe>{" "}
+            </Grid>
+
+            {shownHeadlines.map((headline) => (
+              <Grid item key={headline.id}>
+                <HeadlineCard headlineData={headline} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>right</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
