acc7f826969d5f9a508f5e65eea58ef0656fa0a1
Gabe Granado <gabegranado@Gabes-MacBook-Pro.local>
2024-04-20 17:40:02 -0500

    added login functionality and saved to cookies

--- frontend/src/components/signin-up/Login.jsx
+++ frontend/src/components/signin-up/Login.jsx
@@ -3,6 +3,11 @@ import { loginFields } from "../../constants/signin-up/FormFields";
 import Input from "./Input";
 import FormAction from "./FormAction";
 import FormExtra from "./FormExtra";
+import axios from "axios";
+import { useSignIn } from "react-auth-kit";
+import { Cookies } from "react-cookie";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const fields = loginFields;
 let fieldsState = {};
@@ -10,6 +15,11 @@ fields.forEach((field) => (fieldsState[field.id] = ""));
 
 export default function Login() {
   const [loginState, setLoginState] = useState(fieldsState);
+  const [error, setError] = useState("");
+  const signIn = useSignIn();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const cookies = new Cookies();
 
   const handleChange = (e) => {
     setLoginState({ ...loginState, [e.target.id]: e.target.value });
@@ -20,9 +30,34 @@ export default function Login() {
     authenticateUser();
   };
 
-  //Handle Login API Integration here
-  const authenticateUser = () => {
-    
+  const authenticateUser = async () => {
+    setError("");
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/users/login",
+        {
+          identifier: loginState["email-address"],
+          password: loginState.password
+        }
+      );
+      signIn({
+        token: response.data.token,
+        expiresIn: 3600,
+        tokenType: "Bearer",
+        authState: { identifier: response.data.username },
+      });
+      const username = cookies.get("_auth_state").identifier;
+      console.log("COOKKIIES", cookies.get("_auth_state").identifier);
+      navigate(`/home`);
+
+    } catch (err) {
+      console.log("error: ", err);
+      if (err.response && err.response.status === 400) {
+        setError("Password or email do not match.");
+      } else {
+        setError("An error occurred. Please try again later."); 
+      }
+    }
   };
 
   return (
@@ -43,7 +78,7 @@ export default function Login() {
           />
         ))}
       </div>
-
+      {error && <p className="text-red-500">{error}</p>}
       <FormExtra />
       <FormAction handleSubmit={handleSubmit} text="Login" />
     </form>
